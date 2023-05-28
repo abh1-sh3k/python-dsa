b80dea20577416df506c74e7e1688aaca3474cef
Abhishek <dovalabhi01@gmail.com>
2023-05-28 18:46:44 +0530

    last occurence of element function revised

--- search/index_of_last_occurence.py
+++ search/index_of_last_occurence.py
@@ -3,10 +3,13 @@ def element_last_index(arr: list[int], x: int) -> int:
     high = len(arr) - 1
     while low <= high:
         mid = (low + high) // 2
-        if arr[mid] == x and arr[mid + 1] != x:
-            return mid
-        elif arr[mid] > x:
+        if arr[mid] > x:
             high = mid - 1
-        else:
+        elif arr[mid] < x:
             low = mid + 1
+        else:
+            if mid == len(arr) - 1 or arr[mid] != arr[mid+1]:
+                return mid
+            else:
+                low = mid + 1
     return -1
